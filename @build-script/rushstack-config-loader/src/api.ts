ebdb4a9a192e4ba3222e428bc69636a889f0e41d
GongT <admin@gongt.me>
2022-07-27 17:42:22 +0800

    [rushstack-config-loader] update MODULE_NOT_FOUND check

--- @build-script/rushstack-config-loader/src/api.ts
+++ @build-script/rushstack-config-loader/src/api.ts
@@ -12,7 +12,10 @@ import type { ITypeScriptConfigurationJson } from '@rushstack/heft/lib/plugins/T
 const cache = new WeakMap<HeftConfiguration, RushStackConfig>();
 
 const isModuleResolutionError = (ex: any) =>
-	typeof ex === 'object' && !!ex && 'code' in ex && ex.code === 'MODULE_NOT_FOUND';
+	typeof ex === 'object' &&
+	!!ex &&
+	'code' in ex &&
+	(ex.code === 'MODULE_NOT_FOUND' || ex.code === 'ERR_MODULE_NOT_FOUND');
 
 export function loadHeftConfig(heftConfiguration: HeftConfiguration): RushStackConfig {
 	if (!cache.has(heftConfiguration)) {
