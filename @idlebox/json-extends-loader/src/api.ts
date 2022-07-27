b8c2c344128aef1ae750cc09ada0cf41d1db560c
GongT <admin@gongt.me>
2022-07-27 17:40:06 +0800

    [json-extends-loader] update MODULE_NOT_FOUND check

--- @idlebox/json-extends-loader/src/api.ts
+++ @idlebox/json-extends-loader/src/api.ts
@@ -102,7 +102,10 @@ function overwriteMerge(_destinationArray: any[], sourceArray: any[], _options: 
 	return sourceArray;
 }
 const isModuleResolutionError = (ex: any) =>
-	typeof ex === 'object' && !!ex && 'code' in ex && ex.code === 'MODULE_NOT_FOUND';
+	typeof ex === 'object' &&
+	!!ex &&
+	'code' in ex &&
+	(ex.code === 'MODULE_NOT_FOUND' || ex.code === 'ERR_MODULE_NOT_FOUND');
 
 function nodeResolution(current: string, id: string): string {
 	const req = createRequire(current);
