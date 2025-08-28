7b8e25e9adab345aef7e28e83a8bda015c0d4fd7
GongT (action bot) <admin@gongt.me>
2025-08-28 09:56:51 +0000

    chore: update package versions [skip ci]
     * idlebox-cli-help-builder-0.0.4
     * idlebox-args-0.0.18
     * build-script-esbuild-custom-writer-0.1.12
     * idlebox-deepmerge-0.0.6
     * idlebox-node-error-codes-0.1.9
     * mpis-client-0.0.10
     * cjke-strings-1.1.10
     * idlebox-node-1.4.16
     * mpis-server-0.0.9
     * idlebox-common-1.5.1
     * mpis-run-0.0.12
     * idlebox-unshare-0.0.5
     * idlebox-cli-0.0.4
     * idlebox-stripe-node-types-24.0.14-patch.0
     * mpis-monorepo-0.0.9
     * idlebox-dependency-injection-0.0.1
     * idlebox-typescript-surface-analyzer-0.0.30
     * idlebox-json-edit-0.0.6
     * idlebox-logger-0.0.10
     * build-script-rushstack-config-loader-0.0.27
     * mpis-esbuild-0.0.9
     * mpis-typescript-0.0.13
     * mpis-publisher-0.0.10
     * idlebox-errors-0.1.8
     * idlebox-tsconfig-loader-1.0.23
     * build-script-autoindex-0.0.10
     * unipm-1.1.41
     * idlebox-esbuild-executer-0.0.8
     * gongt-systemd-unit-generator-0.0.16
     * idlebox-source-map-support-0.0.10
     * idlebox-cli-static-generator-0.0.4
     * build-script-codegen-1.0.10
     * idlebox-json-extends-loader-1.0.25
     * idlebox-ignore-edit-0.0.9

--- @idlebox/node-error-codes/src/nodejs-error-code.generated.ts
+++ @idlebox/node-error-codes/src/nodejs-error-code.generated.ts
@@ -144,9 +144,14 @@ export enum NodeErrorCode {
 	 * when an error occurs (and is caught) during the creation of the
 	 * context, for example, when the allocation fails or the maximum call stack
 	 * size is reached when the context is created.</p>
-	 * <p><a id="ERR_CRYPTO_CUSTOM_ENGINE_NOT_SUPPORTED"></a></p>
+	 * <p><a id="ERR_CRYPTO_ARGON2_NOT_SUPPORTED"></a></p>
 	 */
 	ERR_CONTEXT_NOT_INITIALIZED = 'ERR_CONTEXT_NOT_INITIALIZED',
+	/**
+	 * <p>Argon2 is not supported by the current version of OpenSSL being used.</p>
+	 * <p><a id="ERR_CRYPTO_CUSTOM_ENGINE_NOT_SUPPORTED"></a></p>
+	 */
+	ERR_CRYPTO_ARGON2_NOT_SUPPORTED = 'ERR_CRYPTO_ARGON2_NOT_SUPPORTED',
 	/**
 	 * <p>An OpenSSL engine was requested (for example, through the <code>clientCertEngine</code> or
 	 * <code>privateKeyEngine</code> TLS options) that is not supported by the version of OpenSSL
@@ -297,9 +302,15 @@ export enum NodeErrorCode {
 	/**
 	 * <p>Key's Asymmetric Key Type is not registered for use in the
 	 * <a href="https://www.iana.org/assignments/jose/jose.xhtml#web-key-types">JSON Web Key Types Registry</a>.</p>
-	 * <p><a id="ERR_CRYPTO_OPERATION_FAILED"></a></p>
+	 * <p><a id="ERR_CRYPTO_KEM_NOT_SUPPORTED"></a></p>
 	 */
 	ERR_CRYPTO_JWK_UNSUPPORTED_KEY_TYPE = 'ERR_CRYPTO_JWK_UNSUPPORTED_KEY_TYPE',
+	/**
+	 * <p>Attempted to use KEM operations while Node.js was not compiled with
+	 * OpenSSL with KEM support.</p>
+	 * <p><a id="ERR_CRYPTO_OPERATION_FAILED"></a></p>
+	 */
+	ERR_CRYPTO_KEM_NOT_SUPPORTED = 'ERR_CRYPTO_KEM_NOT_SUPPORTED',
 	/**
 	 * <p>A crypto operation failed for an otherwise unspecified reason.</p>
 	 * <p><a id="ERR_CRYPTO_PBKDF2_ERROR"></a></p>
