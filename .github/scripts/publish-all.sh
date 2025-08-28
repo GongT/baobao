43a1489978faf9969305f4a03e9fbc9201a711ef
GongT <admin@gongt.me>
2025-08-28 17:46:04 +0800

    [checkpoint] add publish tag (ci)

--- .github/scripts/publish-all.sh
+++ .github/scripts/publish-all.sh
@@ -10,6 +10,6 @@ echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
 mapfile -t packages < <(find . -name '*.tgz')
 for pkg in "${packages[@]}"; do
 	echo "::group::Publishing $pkg..."
-	pnpm publish "$pkg"
+	pnpm publish --tag latest "$pkg"
 	echo "::endgroup::"
 done
