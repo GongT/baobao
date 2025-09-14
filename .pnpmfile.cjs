b483a121d0294ffa818e1c3cb9829a80f0924701
GongT <admin@gongt.me>
2025-09-14 18:59:48 +0800

    update pnpm version

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -8,23 +8,44 @@ const knownTypesVersion = {};
 
 module.exports = {
 	hooks: {
+		// preResolution,
 		readPackage,
 	},
 };
 
+if (process.env.PNPM_CALL_RECURSIVE) {
+	module.exports.hooks = {};
+}
+
 function init() {
-	const cmds = [process.argv0, require.main.filename, 'multi', 'ls', '--json', '--depth=-1'];
+	const cmds = [];
+	cmds.push(process.execPath);
+	if (!process.execPath.includes('/@pnpm/')) {
+		cmds.push(require.main.filename);
+	}
+	cmds.push('multi', 'ls', '--json', '--depth=-1');
 	console.error(`\x1B[2m+ ${cmds.join(' ')}\x1B[0m`);
 	const result = spawnSync(cmds[0], cmds.slice(1), {
 		encoding: 'utf8',
 		stdio: ['ignore', 'pipe', 'inherit'],
+		env: {
+			PNPM_CALL_RECURSIVE: '1',
+		},
 	});
 	if (result.error) {
 		throw result.error;
 	}
-	const projects = JSON.parse(result.stdout);
 
-	console.log('\x1B[38;5;10m[%s]: there are %d projects in workspace.\x1B[0m', basename(__filename), projects.length);
+	const output = result.stdout.toString().trim();
+
+	try {
+		JSON.parse(output);
+	} catch (e) {
+		throw new Error(`pnpm ls输出不是json (${e.message})\n---------------------------------\n${output}\n---------------------------------`);
+	}
+	const projects = JSON.parse(output);
+
+	console.error('\x1B[38;5;10m[%s]: there are %d projects in workspace.\x1B[0m', basename(__filename), projects.length);
 
 	for (const { path } of projects) {
 		const p = resolve(path, 'package.json');
@@ -53,6 +74,10 @@ function readPackage(packageJson, context) {
 
 	if (packageJson.name === '@internal/local-rig') {
 		addEverythingToDependency(packageJson);
+	}
+
+	if (packageJson.name === '@rollup/plugin-swc') {
+		packageJson.dependencies['@swc/core'] = 'latest';
 	}
 
 	if (myProjects.has(packageJson.name)) return packageJson;
