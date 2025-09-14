44e6852d85018eccb0efce5d23011d72c4498a82
GongT <admin@gongt.me>
2025-09-14 08:18:49 +0800

    @idlebox/common@1.5.5

--- @idlebox/common/src/schedule/timeout.ts
+++ @idlebox/common/src/schedule/timeout.ts
@@ -1,8 +1,9 @@
 import { TimeoutError } from '@idlebox/errors';
+import { isNodeJs } from '../platform/os.js';
 
 /**
  * @param unref defaults to `false`, when true, call `unref()` on the timer.
- *            can not set to `true` on other platform.
+ *            can not set to `true` on other platform (will throw "unref is not function" error).
  * @returns promise reject with TimeoutError after specific time
  */
 export function timeout(ms: number, error = 'no response', unref = false): Promise<never> {
@@ -39,11 +40,10 @@ export function raceTimeout<T>(ms: number, message: string, p: PromiseLike<T>): 
  * @param p the promise
  * @returns
  */
-export function raceTimeout<T>(ms: number, message: string | PromiseLike<T>, p?: PromiseLike<T>): Promise<T> {
-	let msg: string | undefined;
+export function raceTimeout<T>(ms: number, message_or_p: string | PromiseLike<T>, p?: PromiseLike<T>): Promise<T> {
 	if (p) {
-		return Promise.race([p, timeout(ms, msg, true)]);
+		return Promise.race([p, timeout(ms, message_or_p as string, isNodeJs)]);
 	} else {
-		return Promise.race([message as any, timeout(ms, msg, true)]);
+		return Promise.race([message_or_p as PromiseLike<T>, timeout(ms, undefined, isNodeJs)]);
 	}
 }
